3565ec5c89b9824bd21ffb4f48a03fa1822439df
ykt0518 <ari.stee.ykt@icloud.com>
2025-11-16 16:11:54 +0900

    Update shadcn/ui導入

--- app/pages/home/index.tsx
+++ app/pages/home/index.tsx
@@ -1,9 +1,14 @@
+import { Button } from "~/components/ui/button";
+
 export function Home() {
   return (
-    <div className="max-w-[800px] w-full mx-auto bg-gray-100 p-4 rounded-lg">
-      <h2 className="text-4xl font-bold text-gray-900 text-center">
-        Homeです。
-      </h2>
+    <div className="p-10">
+      <div className="max-w-[800px] w-full mx-auto bg-gray-100 p-4 rounded-lg">
+        <h2 className="text-4xl font-bold text-gray-900 text-center">
+          Homeです。
+        </h2>
+      </div>
+      <Button>Click me</Button>
     </div>
   );
 }
