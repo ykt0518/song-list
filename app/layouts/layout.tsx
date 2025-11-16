1c60e25e33b34c09044c4c90394c7df72e41c5a9
ykt0518 <ari.stee.ykt@icloud.com>
2025-11-16 22:31:38 +0900

    Update ルート設定

---
+++ app/layouts/layout.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@/components/Layouts/header";
+
+type LayoutProps = {
+  // isAuthenticated: boolean;
+  children: React.ReactNode;
+};
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="grow grid">{children}</div>
+    </div>
+  );
+}
