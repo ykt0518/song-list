6c92982532281ef244375919d43cd8918894a009
ykt0518 <ari.stee.ykt@icloud.com>
2025-11-16 16:00:37 +0900

    Update 初期構造設計

---
+++ app/pages/home/index.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <div className="max-w-[800px] w-full mx-auto bg-gray-100 p-4 rounded-lg">
+      <h2 className="text-4xl font-bold text-gray-900 text-center">
+        Homeです。
+      </h2>
+    </div>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
+import { Home as Presenter } from "../pages/home";
 
 export default function Home() {
-  return <Welcome />;
+  return <Presenter />;
 }
